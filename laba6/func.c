bb49e580dc7cd972b4a809780c3680b3b1608ed6
chibixar <minkevicharkadz@gmail.com>
2025-12-05 17:06:50 +0300

    Clean code

--- laba6/func.c
+++ laba6/func.c
@@ -93,53 +93,11 @@ int compare_first_letter(const char* a, const char* b)          //калі а < 
     return (unsigned char)*a - (unsigned char)*b;
 }
 
-// int compare_first_letter(char **)
-
 void swap_str(char** a, char** b) {
     char* temp = *a;
     *a = *b;
     *b = temp;
 }
-
-int partition_name(char** text, int low, int high) {
-    char *p = *(text+low);  
-    int i = low;
-    int j = high;
-printf("Debug: i:%d  j:%d", i, j);
-    while (i < j) {
-
-        while (compare_first_letter(*(text+i), p) < 0 && i <= high - 1) {
-            i++;
-printf("Debug: i:%d  j:%d", i, j);
-
-        }
-
-        while (compare_first_letter(*(text+j), p) > 0 && j >= low + 1) {
-            j--;
-printf("Debug: i:%d  j:%d", i, j);
-
-        }
-
-        if (i < j) {
-            swap_str(text+i, text+j);
-        }
-    }
-
-    swap_str(text+low, text+j);
-printf("Debug: i:%d  j:%d\n", i, j);
-
-    return j;
-}
-
-void Quick_Sort_Alphabetically(char** text, int low, int high) {
-    if (low < high)
-    {
-        int pi = partition_name(text, low, high);
-        Quick_Sort_Alphabetically(text, low, pi - 1);
-        Quick_Sort_Alphabetically(text, pi + 1, high);
-    }
-}
-
 
 
 
@@ -249,66 +207,4 @@ void QuickSort_Salary(char ** text, int low, int high, int* digital_sum)
 
 
 
-
-
-
-// text_sort_by_salary(char** text, int number_of_strings, int max_string_size, int digital_sum)
-// {
-
-// }
-
-// void text_digit_word(char** text, int* sum)    
-// {
-//     int i = 0, j = 0;                                               
-//     while(text[i] == ' ') i++;                                    //прапускаем пачатковыя прабелы.
-
-//     while (text[i] != '\0')                                       //пакуль не канец радка.
-//     {
-//         int k = i;
-//         while(text[k] != '\0' && text[k] != ' ') k++;
-//         int len = k-i;
-//         if(word_isdigit(text, sum, i))                            //калі слова складаецца з лічбаў.
-//             i = k;                                                  //пераскокваем гэта слова.
-//         else 
-//         {
-//             for (int t = 0; t< len; ++t)
-//                 text[j++]=text[i+t];
-//             i=k;
-//             while (text[i]==' ') i++;                             //пропуск прабелаў паміж словамі.
-//             if (text[i]!='\0')
-//                 text[j++] = ' ';
-            
-            
-//         }
-                                    
-//     }
-//     text[j]='\0';                                                 //ставім канец радка.
-
-//     if (j>0) strcat(text, " ");                                   //дадаем прабел перад сумай.
-
-//     char buf[32];                                                   
-//     sprintf(buf, "%d", *sum);                                       //пераўтвараем суму ў радок.
-//     strcat(text, buf);                                            //дапаўняем радок сумай.
-// }
-
-// int word_isdigit(char** text, int* sum, int i)   
-// {
-//     int k = i;                                                      
-//     int temp_sum = 0;                                                                                             
-
-//     if (text[i] == ' ' || text[i] == '\0')                      //калі пустоца або канец радка.
-//         return 0;                                                   
-
-//     for (; text[k] != '\0' && text[k] != ' '; k++)              //пакуль не канец слова.
-//     {
-//         if (!char_isdigit(text, k))                               //калі хоць адзін сімвал не лічба.
-//             return 0;                                               
-//         temp_sum += (text[k] - '0');                              //дадаём лічбу да сумы. НЕ АСКІ
-                
-//     }
-                                                   
-
-//     *sum += temp_sum;                                               //дадаём у агульную суму.
-//     return 1;                                                       //слова складаецца з лічбаў.
-// }
 
--- laba6/header.h
+++ laba6/header.h
@@ -17,8 +17,6 @@ char** text_input(char** text, int number_of_strings, int max_string_size);
 void output_text(char** text, int number_of_strings);
 void swap_str(char** a, char** b);
 int compare_first_letter(const char* a, const char* b);
-int partition_name(char** text, int low, int high);
-void Quick_Sort_Alphabetically(char** text, int low, int high);
 void QuickSort_Name(char ** text, int low, int high);
 
 int* digital_sum_memory_allocation(int *digital_sum, int number_of_strings);
